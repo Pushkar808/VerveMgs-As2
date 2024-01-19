84ed7ff6d7765660e5fa877efc42af784e7b8bd1
pushkar gupta <pushkargupta@youngangels.local>
2024-01-19 15:50:39 +0530

    added

--- src/components/number.js
+++ src/components/number.js
@@ -4,25 +4,25 @@ export const Number = () => {
     return (
         <div className="flex md:flex-row flex-col items-center justify-center mt-8 text-center w-full">
            
-            <div className="bg-white p-7 mx-8 rounded-lg shadow-md w-40 ">
+            <div className="bg-white p-7 mx-8 rounded-lg shadow-md w-40 my-1 ">
                 <h2 className="text-2xl font-bold text-gray-800">30+</h2>
                 <p className="text-gray-500">Speakers</p>
             </div>
 
       
-            <div className="bg-white p-6 mx-8 rounded-lg shadow-md w-40">
+            <div className="bg-white p-6 mx-8 rounded-lg shadow-md w-40 my-1">
                 <h2 className="text-2xl font-bold text-gray-800">2 Days</h2>
                 <p className="text-gray-500">Event</p>
             </div>
 
            
-            <div className="bg-white p-6 mx-8 rounded-lg shadow-md w-40">
+            <div className="bg-white p-6 mx-8 rounded-lg shadow-md w-40 my-1">
                 <h2 className="text-2xl font-bold text-gray-800">400+</h2>
                 <p className="text-gray-500">Delegates</p>
             </div>
 
           
-            <div className="bg-white p-6 mx-8 rounded-lg shadow-md w-40">
+            <div className="bg-white p-6 mx-8 rounded-lg shadow-md w-40 my-1">
                 <h2 className="text-2xl font-bold text-gray-800">10+</h2>
                 <p className="text-gray-500">Hours Of Networking</p>
             </div>
